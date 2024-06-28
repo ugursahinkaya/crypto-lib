36fc83a55eef07bc5cfe1a25d5d7c629babe15a5
Uğur Şahinkaya <ugur@sahinkaya.xyz>
2024-06-28 19:09:14 +0300

    refactor: map based keys

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,36 @@
-export type CryptoKeyPair = CryptoKeyPair & {
-  secret?: CryptoKey;
-  jwk?: string;
-};
-export type LiteralUnion<T extends U, U = string> = T | (U & Record<never, never>);
-export type KeyOwner = LiteralUnion<'server', string>;
 export type EncryptionTuple = [ciphertext: ArrayBuffer, iv: Uint8Array];
-export class CryptoLib {
-  keys: { [key in KeyOwner]?: CryptoKeyPair } = {};
+export type Consumer = "server" | string;
+import { webcrypto } from "crypto";
 
-  protected crypto: Crypto;
-  protected encodeMessage(message: string) {
+export class CryptoLib {
+  keyMap = new Map<string, CryptoKey>();
+  protected crypto: Crypto = webcrypto as unknown as Crypto;
+
+  protected storeKeys(consumer: Consumer, keys: CryptoKeyPair) {
+    this.keyMap.set(`${consumer}PRV`, keys.privateKey);
+    this.keyMap.set(`${consumer}PBL`, keys.publicKey);
+  }
+
+  encodeMessage(message: string) {
     const encoder = new TextEncoder();
     return encoder.encode(message);
   }
 
-  protected mergeBuffers(buffer1: ArrayBuffer, buffer2: ArrayBuffer) {
+  mergeBuffers(buffer1: ArrayBuffer, buffer2: ArrayBuffer) {
     const result = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
     result.set(new Uint8Array(buffer1), 0);
     result.set(new Uint8Array(buffer2), buffer1.byteLength);
     return result.buffer;
   }
 
-  protected prepareBuffer(buffer: ArrayBufferLike) {
+  prepareBuffer(buffer: ArrayBufferLike) {
     const length = buffer.byteLength;
     const data = new Uint8Array(buffer.slice(12, length));
     const tag = new Uint8Array(buffer.slice(length - 16, length));
     const nonce = new Uint8Array(buffer.slice(0, 12));
     return [data, tag, nonce] as const;
   }
-  constructor(crypto?: Crypto) {
-    if (!crypto) {
-      throw new Error('Provide a Web Crypto API implementation');
-    }
-    this.crypto = crypto;
-  }
 
-  randomString() {
-    return Math.random().toString(36).substring(2, 15);
-  }
   randomBytes(length: number) {
     return this.crypto.getRandomValues(new Uint8Array(length));
   }
@@ -45,14 +38,14 @@ export class CryptoLib {
   async sha256(message: string) {
     const encoder = new TextEncoder();
     const data = encoder.encode(message);
-    const hashBuffer = await this.crypto.subtle.digest('SHA-256', data);
+    const hashBuffer = await this.crypto.subtle.digest("SHA-256", data);
     const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map((byte) => byte.toString(16).padStart(2, '0')).join('');
+    const hashHex = hashArray.map((byte) => byte.toString(16).padStart(2, "0")).join("");
     return hashHex;
   }
 
-  hasSecret(receiver: string) {
-    return this.keys[receiver] && this.keys[receiver]?.secret;
+  hasSecret(consumer: string) {
+    return this.keyMap.get(`${consumer}SCR`) ? true : false;
   }
 
   base64ToArrayBuffer(base64: string) {
@@ -66,7 +59,7 @@ export class CryptoLib {
   }
 
   arrayBufferToBase64(buffer: ArrayBuffer) {
-    let binaryString = '';
+    let binaryString = "";
     const bytes = new Uint8Array(buffer);
     for (const byte of bytes) {
       binaryString += String.fromCharCode(byte);
@@ -74,140 +67,110 @@ export class CryptoLib {
     return btoa(binaryString);
   }
 
-  async setSecretSalt(receiver: string, salt: ArrayBufferLike) {
-    const keys = this.keys[receiver];
-    if (!keys || !keys.secret) {
+  async setSecretSalt(consumer: string, salt: ArrayBufferLike) {
+    const secret = this.keyMap.get(`${consumer}SCR`);
+    if (!secret) {
       return;
     }
-    const oldKey = await this.crypto.subtle.exportKey('raw', keys.secret);
+    const oldKey = await this.crypto.subtle.exportKey("raw", secret);
     const bufferSource = new Uint8Array(oldKey);
     const keyMaterial = await this.crypto.subtle.importKey(
-      'raw',
+      "raw",
       bufferSource,
-      { name: 'HKDF' },
+      { name: "HKDF" },
       false,
-      ['deriveKey']
+      ["deriveKey"]
     );
 
-    keys.secret = await this.crypto.subtle.deriveKey(
+    const newSecret = await this.crypto.subtle.deriveKey(
       {
-        name: 'HKDF',
-        hash: { name: 'SHA-256' },
+        name: "HKDF",
+        hash: { name: "SHA-256" },
         salt: salt,
         info: salt
       },
       keyMaterial,
-      { name: 'AES-GCM', length: 256 },
+      { name: "AES-GCM", length: 256 },
       true,
-      ['encrypt', 'decrypt']
+      ["encrypt", "decrypt"]
     );
 
-    return keys.secret;
+    this.keyMap.set(`${consumer}SCR`, newSecret);
+
+    return newSecret;
   }
 
-  async generateKey(receiver: KeyOwner) {
+  async generateKey(consumer: Consumer) {
     const keys = await this.crypto.subtle.generateKey(
       {
-        name: 'ECDH',
-        namedCurve: 'P-256'
+        name: "ECDH",
+        namedCurve: "P-256"
       },
       true,
-      ['deriveKey']
+      ["deriveKey"]
     );
-
-    this.keys[receiver] = keys;
+    this.storeKeys(consumer, keys);
   }
 
-  async exportKey(receiver: KeyOwner) {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return await this.crypto.subtle.exportKey('spki', this.keys[receiver]!.publicKey);
+  async exportKey(consumer: Consumer) {
+    const publicKey = this.keyMap.get(`${consumer}PBL`);
+    if (!publicKey) {
+      throw new Error("KeyOwner has not public key");
+    }
+    return await this.crypto.subtle.exportKey("spki", publicKey);
   }
 
-  async importPublicKey(publicKeyData: BufferSource, receiver: KeyOwner) {
+  async importPublicKey(publicKeyData: BufferSource, consumer: Consumer) {
+    const consumerPublicKey = this.keyMap.get(`${consumer}PBL`);
+    if (!consumerPublicKey) {
+      throw new Error("public key not found");
+    }
+
+    const consumerPrivateKey = this.keyMap.get(`${consumer}PRV`);
+    if (!consumerPrivateKey) {
+      throw new Error("private key not found");
+    }
+
     const publicKey = await this.crypto.subtle.importKey(
-      'spki',
+      "spki",
       publicKeyData,
       {
-        name: 'ECDH',
-        namedCurve: 'P-256'
+        name: "ECDH",
+        namedCurve: "P-256"
       },
       false,
       []
     );
-    const keys = this.keys[receiver];
-    if (!keys) {
-      throw new Error(
-        'No key pair found for receiver, generate one first by calling "generateKey"'
-      );
-    }
 
     const derivedKey = await this.crypto.subtle.deriveKey(
       {
-        name: 'ECDH',
+        name: "ECDH",
         public: publicKey
       },
-      keys.privateKey,
+      consumerPrivateKey,
       {
-        name: 'AES-GCM',
+        name: "AES-GCM",
         length: 256
       },
       true,
-      ['encrypt', 'decrypt']
+      ["encrypt", "decrypt"]
     );
-    keys.secret = derivedKey;
 
-    const exported: JsonWebKey = await this.crypto.subtle.exportKey('jwk', derivedKey);
-    keys.jwk = JSON.stringify(exported);
+    this.keyMap.set(`${consumer}SCR`, derivedKey);
 
     return derivedKey;
   }
 
-  async importJwk(jwk: JsonWebKey, user: KeyOwner): Promise<boolean> {
-    const secret = await this.crypto.subtle.importKey(
-      'jwk',
-      jwk,
-      {
-        name: 'AES-GCM'
-      },
-      false,
-      ['encrypt', 'decrypt']
-    );
-
-    // ?
-    if (user === 'server') {
-      if (this.keys.server === undefined) {
-        await this.generateKey('server');
-      }
-
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      this.keys.server!.secret = secret;
-
-      return true;
-    }
-
-    if (this.keys[user] === undefined) {
-      this.keys[user] = {} as CryptoKeyPair;
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    this.keys[user]!.secret = secret;
-
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    this.keys[user]!.jwk = JSON.stringify(jwk);
-
-    return false;
-  }
-
-  async encrypt(data: string, receiver: KeyOwner, b64: true): Promise<string>;
-  async encrypt(data: string, receiver?: KeyOwner, b64?: false): Promise<EncryptionTuple>;
+  async encrypt(data: string, consumer: Consumer, b64: true): Promise<string>;
+  async encrypt(data: string, consumer?: Consumer, b64?: false): Promise<EncryptionTuple>;
   async encrypt(
     data: string,
-    receiver: KeyOwner = 'server',
+    consumer: Consumer = "server",
     b64?: boolean
   ): Promise<string | EncryptionTuple> {
-    const { secret } = this.keys[receiver] as CryptoKeyPair;
-    if (secret === undefined) {
-      return data;
+    const secret = this.keyMap.get(`${consumer}SCR`);
+    if (!secret) {
+      throw new Error("secret key not found");
     }
 
     const encoded = this.encodeMessage(data);
@@ -215,12 +178,13 @@ export class CryptoLib {
 
     const ciphertext = await this.crypto.subtle.encrypt(
       {
-        name: 'AES-GCM',
+        name: "AES-GCM",
         iv
       },
       secret,
       encoded
     );
+
     if (b64) {
       return this.arrayBufferToBase64(this.mergeBuffers(iv, ciphertext));
     }
@@ -234,19 +198,21 @@ export class CryptoLib {
     sender?: string
   ): Promise<Record<string, unknown> | string> {
     const [data, tag, nonce] = this.prepareBuffer(buffer);
-    const rawPayload = await this.decrypt(data, tag, nonce, sender ?? 'server');
+    const rawPayload = await this.decrypt(data, tag, nonce, sender ?? "server");
     if (jsonParse) {
       return JSON.parse(rawPayload) as Record<string, unknown>;
     }
     return rawPayload;
   }
 
-  async decrypt(data: BufferSource, _tag: BufferSource, nonce: BufferSource, receiver: KeyOwner) {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const secret = this.keys[receiver]!.secret!;
+  async decrypt(data: BufferSource, _tag: BufferSource, nonce: BufferSource, consumer: Consumer) {
+    const secret = this.keyMap.get(`${consumer}SCR`);
+    if (!secret) {
+      throw new Error("secret key not found");
+    }
     const decrypted = (await this.crypto.subtle.decrypt(
       {
-        name: 'AES-GCM',
+        name: "AES-GCM",
         iv: nonce
       },
       secret,
