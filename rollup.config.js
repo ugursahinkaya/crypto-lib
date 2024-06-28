8d807b740a19d5d69bf70d6c3362eab49a1b329e
Uğur Şahinkaya <ugur@sahinkaya.xyz>
2024-06-28 19:08:39 +0300

    refactor: edit base config files

---
+++ rollup.config.js
@@ -0,0 +1,61 @@
+import typescript from "rollup-plugin-typescript2";
+import dts from "rollup-plugin-dts";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import terser from "@rollup/plugin-terser";
+import replace from "@rollup/plugin-replace";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.cjs",
+      format: "cjs"
+    },
+    plugins: [
+      typescript({
+        tsconfig: "./tsconfig.json"
+      }),
+      terser()
+    ]
+  },
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "es"
+      },
+      {
+        file: "dist/crypto-lib.umd.js",
+        name: "YUM",
+        extend: true,
+        format: "umd"
+      }
+    ],
+    plugins: [
+      replace({
+        preventAssignment: true,
+        delimiters: ["", ""],
+        'import { webcrypto } from "crypto";': "",
+        "webcrypto as": "window.crypto as"
+      }),
+      typescript({
+        tsconfig: "./tsconfig.json"
+      })
+
+      //terser()
+    ]
+  },
+  {
+    input: resolve(__dirname, "dist/index.d.ts"),
+    output: {
+      file: "dist/index.d.ts",
+      format: "es"
+    },
+    plugins: [dts()]
+  }
+];
